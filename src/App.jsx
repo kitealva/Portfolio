cb33348679bbc042ab542541f0632072642e72db
Brady <kite.alva@gmail.com>
2023-07-01 15:57:01 -0600

    removed-three-library

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works } from './components';
 
 
 
@@ -19,7 +19,6 @@ const App = () => {
         <Feedbacks />
         <div className="relative z-0">
             <Contact />
-            <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import { EarthCanvas, BallCanvas, StarsCanvas } from './canvas';
+
 import Hero from './Hero';
 import Navbar from './Navbar';
 import About from './About';
@@ -16,8 +16,5 @@ export {
   Experience,
   Works,
   Feedbacks,
-  Contact,
-  EarthCanvas, 
-  BallCanvas, 
-  StarsCanvas
+  Contact
 }
